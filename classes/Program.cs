b0574b8c19696c43a08b45aa1152124102299b8e
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-08-30 17:14:28 +0530

    Add project files.

---
+++ classes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+
+    class Customer
+    {
+        string _firstName;
+        string _lastName;
+
+        public Customer(string FirstName, string LastName)
+        {
+           this._firstName = FirstName;
+            this._lastName = LastName;
+        }
+
+        public void FullName()
+        {
+            Console.WriteLine("My name is {0}",this._firstName + " " + this._lastName);
+            Console.ReadLine();
+        }
+
+        ~Customer()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer("Darshana","Senevirathna");
+            c1.FullName();
+        }
+    }
+}
